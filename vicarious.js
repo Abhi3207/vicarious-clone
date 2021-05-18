8162e57eff84074692895f1c049022985420e91e
Abhi3207 <79158225+Abhi3207@users.noreply.github.com>
2021-05-18 09:13:09 +0530

    Add files via upload

---
+++ vicarious.js
@@ -0,0 +1,7 @@
+let i;
+let it=document.getElementsByClassName("div51").getElementByClassName("div511").getElementsByTagName("li");
+let vi=document.getElementByClassName("video5").getElementsByTagName("video");
+
+it[i].addEventListener(onmouseover,function(){
+	vi[i].play;	
+})
