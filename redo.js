c91c27bb522cb9bc673d193d91aecc00ed027e97
Abhi3207 <79158225+Abhi3207@users.noreply.github.com>
2021-05-21 21:07:55 +0530

    Add files via upload

---
+++ redo.js
@@ -0,0 +1,12 @@
+ let  x =document.getElementById("solutions");
+
+
+
+function myFunction() {
+  if (x.style.display = "none") {
+    x.style.display = "block";
+  }
+   else {
+    x.style.display = "none";
+  }
+}
